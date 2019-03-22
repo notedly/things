a58096dc05193ca1842f0b4f3a40fdd5701efb6f
megaton <lkh890111@gmail.com>
2019-03-22 11:59:25 +0900

    저장

---
+++ html/js/button_hover.js
@@ -0,0 +1,14 @@
+
+
+
+class Twave{
+	construtor( item ){
+		console.log( item ) ;
+		console.log( 'Twave in') ;
+	}
+}
+
+window.addEventListener('load', () => {
+	let twave = new Twave( 'a' ) ;
+
+}) ;
